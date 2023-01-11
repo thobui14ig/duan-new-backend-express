ccfb08802cb7b13d022763c1c0df903473f29c52
thobui14ig <buithanhtho14ig@gmail.com>
2023-01-11 23:02:33 +0700

    fix auth monggo

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root';
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '111111';
-const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017`;
-const MONGO_DB ='asana'
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'root';
+const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.0aue7m9.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_DB ='asana';
 
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
 
